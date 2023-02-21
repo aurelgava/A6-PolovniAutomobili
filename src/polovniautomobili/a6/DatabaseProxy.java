58dda5821eccf8ffe7746c467be9de08b3305ad1
aurelgava <gava1977@gmail.com>
2023-02-21 10:14:33 +0100

    initial

---
+++ src/polovniautomobili/a6/DatabaseProxy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polovniautomobili.a6;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class DatabaseProxy {
+    public static final String URLBAZE = "jdbc:ucanaccess://src\\resursi\\Polovni.accdb";
+}
